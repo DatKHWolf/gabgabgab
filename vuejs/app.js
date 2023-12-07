90f45a332efd374ba08fcb5b30441ce2ce6fef3e
DatKHWOLF <dkhwolf@gmail.com>
2023-12-07 13:44:09 +0100

    vuejs paar neue Sachen

---
+++ vuejs/app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    // Optionen 
+    data: function(){
+        return {
+            submissions: submissions // aus seed.js
+        }
+    },
+    methods: {
+        upvote(){
+            this.submissions[0].votes++
+        },
+    }
+})
+
+// Liefert die Instanz zur Root-Component zurück
+const vm = app.mount('#app')
